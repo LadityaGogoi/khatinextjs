7d15fef2626431092fdf2205b4967db3a7885492
Chao Lobi <gogoiladitya@gmail.com>
2025-03-09 15:47:01 +0530

    intial setup with nextjs, tailwind, shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div>welcome</div>
+    <div className="flex flex-col h-full w-full justify-center items-center">
+      <div className="text-center text-3xl">khati Inc.</div>
+      <Button>download</Button>
     </div>
   );
 }
